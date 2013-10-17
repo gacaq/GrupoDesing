2396ed3bdf81cdd7f3982f352f8f12d4ddd19daa
gacaq <gcarranza.q@gmail.com>
2013-10-17 15:32:45 -0500

    Envio prueba
    Prueba de git con el blog de ejercicio en rails. Tags no funcionan

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '1ba1317b049995ed0ff789254142e91e55e751050c1f3b289fb4be3e5133414e632a326d013b164efa6f8021290b87277df1cf89557ee0075c48d11256f2add6'
